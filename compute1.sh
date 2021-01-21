7f3071e99cc15137c2a7bea0fbf2f94de3fde33b
InduLekha98 <rindulekha09@gmail.com>
2021-01-22 01:40:37 +0530

    computations

---
+++ compute1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash 
+echo "Computation and  Sorting"
+echo "Enter Number "
+echo "Enter First Number"
+read firstNumber
+echo "Enter Second Number"
+read secondNumber
+echo "Enter Third Number"
+read thirdNumber
+c1=1
+c2=2
+c3=3
+c4=4
+echo " 1) A+B*C 2) A*B+C 3) A+B/C 4) A%B*C":
+read choice
+case $choice in 
+	$c1) 
+	resultFirst=$(( $firstNumber + $secondNumber * $thirdNumber ))
+	echo $resultFirst ;;
+	
+	
+	$c2)
+	resultSecond=$(( $firstNumber * $secondNumber + $thirdNumber )) 
+	echo $resultSecond ;;
+
+	$c3)
+	resultThird=$(( $thirdNumber + $firstNumber / $secondNumber )) 
+	echo $resultThird ;;
+
+	$c4)
+	resultFourth=$(( $firstNumber % $secondNumber * $thirdNumber )) 
+	echo $resultFourth ;;
+
+esac
+
+	resultFirst=$(( $firstNumber + $secondNumber * $thirdNumber ))
+    	resultSecond=$(( $firstNumber * $secondNumber + $thirdNumber ))
+        resultThird=$(( $thirdNumber + $firstNumber / $secondNumber ))
+        resultFourth=$(( $firstNumber % $secondNumber * $thirdNumber ))
+
+arr=( $resultFirst $resultSecond $resultThird $resultFourth )
+ 
+
+
