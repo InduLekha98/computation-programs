6a3aeece4e1103a247be346f922bf99d5d5dda8a
InduLekha98 <rindulekha09@gmail.com>
2021-01-22 01:36:10 +0530

    read 3 no

---
+++ threeno.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+echo "Computation and  Sorting"
+echo "Enter Number "
+echo "Enter First Number"
+read firstNumber
+echo "Enter Second Number"
+read secondNumber
+echo "Enter Third Number"
+read thirdNumber
+
