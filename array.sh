be4e2b3a495187ed23a5d21cf95e4b89585055c2
InduLekha98 <rindulekha09@gmail.com>
2021-01-22 01:55:46 +0530

    values from dictionary into array

---
+++ array.sh
@@ -0,0 +1,36 @@
+echo "Computation and  Sorting"
+echo "Enter Number "
+echo "Enter First Number"
+read firstNumber
+echo "Enter Second Number"
+read secondNumber
+echo "Enter Third Number"
+read thirdNumber
+ 
+for (( i=0 ; $i < 4 ; i++ ))
+do 
+	key1=result$i
+	arr1[$key1]=${arr[i]}
+done
+for i in "${arr[@]}"
+do
+	echo $i
+done
+for key in ${!arr1[@]}; 
+do
+    echo ${key} ${arr1[${key}]}
+done
+for (( i1=0 ; $i1 < 4 ; i1++ ))
+do
+for (( i=0 ; $i < 4 ; i++ ))
+do
+	if [ ${arr[i+1]} -lt ${arr[i]} ]
+	then 
+		temp=${arr[i]}
+		arr[i]=${arr[i+1]}
+		arr[i+1]=$temp
+	fi
+done
+done
+
+
