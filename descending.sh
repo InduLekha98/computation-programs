755ce5fa485c9fcfa32f9a6190841623708cfcb5
InduLekha98 <rindulekha09@gmail.com>
2021-01-22 01:59:58 +0530

    descending order

---
+++ descending.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+echo "Computation and  Sorting"
+echo "Enter Number "
+echo "Enter First Number"
+read firstNumber
+echo "Enter Second Number"
+read secondNumber
+echo "Enter Third Number"
+read thirdNumber
+echo "descending Order"
+for i in "${arr[@]}"
+do
+        echo $i
+done
+for (( i1=0 ; $i1 < 4 ; i1++ ))
+do
+for (( i=0 ; $i < 4 ; i++ ))
+do
+        if [ ${arr[i+1]} -gt ${arr[i]} ]
+        then
+                temp=${arr[i]}
+                arr[i]=${arr[i+1]}
+                arr[i+1]=$temp
+        fi
+done
+done
+
